b7b321499d5907bb191cd9ee37cdafcf2f73f75b
Krishrx <kalikrishnas342@gmail.com>
2023-08-24 01:22:12 +0530

    added toasts.need to work on design

--- js/script.js
+++ js/script.js
@@ -52,11 +52,34 @@ function addTask(){
 
     clearFields();
 
+    showToast(`<div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+    </svg>
+    <span class="sr-only">Check icon</span>
+</div>
+<div class="ml-3 text-sm font-normal">You've successfully added the task!</div>
+
+</div>`)
+
 }
 
 function validate(){
     const taskField = document.getElementById('task').value;
-    if(taskField.trim()==='') return false;
+    if(taskField.trim()===''){
+showToast(`<div id="toast-warning" class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
+    </svg>
+    <span class="sr-only">Warning icon</span>
+</div>
+<div class="ml-3 text-sm font-normal">Oops!! task can't be empty!</div>
+</div>`)
+    return false;
+    } 
     else return true;
 }
 
@@ -190,8 +213,12 @@ function updateTasksTable(tasks){
         } 
         else taskTable.appendChild(row);
     });
-    completedTasksCount = (completedTasksCount==0)?'':completedTasksCount;
-    document.getElementById('finishCount').textContent = completedTasksCount;
+    const completeDiv = document.getElementById('completeToggleButton');
+    if(completedTasksCount>0){
+        completeDiv.classList.remove('hidden');
+        document.getElementById('finishCount').textContent = completedTasksCount;
+    }
+    else completeDiv.classList.add('hidden');
 }
 
 let editId = 0;
@@ -226,6 +253,18 @@ function editBtnFn(){
     clearFields();
     toggleAddEditButtons();
     displayTables();
+
+    showToast(`<div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+    </svg>
+    <span class="sr-only">Check icon</span>
+</div>
+<div class="ml-3 text-sm font-normal">Task updated successfully!</div>
+
+</div>`)
 }
   
 function deleteTask() {
@@ -237,6 +276,17 @@ function deleteTask() {
     const updatedTasks = tasks.filter(task => task.id !== taskId);
     localStorage.setItem("toDoTasks", JSON.stringify(updatedTasks));
     displayTables();
+
+    
+showToast(`<div id="toast-danger" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
+    </svg>
+    <span class="sr-only">Error icon</span>
+</div>
+<div class="ml-3 text-sm font-normal">Task removed!</div>
+</div>`)
 }
 
 function cancel(){
@@ -294,6 +344,20 @@ function populateFields(task) {
     }
 }
   
+
+function showToast(message) {
+    const toast = document.getElementById('toast');
+    toast.innerHTML = message;
+    toast.classList.remove('invisible');
+
+    setTimeout(() => {
+      toast.classList.add('invisible');
+    }, 3000);
+  }
+  
+ 
+
+
 
 
 
